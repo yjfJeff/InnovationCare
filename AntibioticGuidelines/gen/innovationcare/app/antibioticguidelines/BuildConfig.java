3ffd8c83eb15f8ef85405834f8848053da4f9114
Jiefeng Yu <yujiefeng87@gmail.com>
2013-07-15 19:32:02 +0100

    amend .gitignore file

--- AntibioticGuidelines/gen/innovationcare/app/antibioticguidelines/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package innovationcare.app.antibioticguidelines;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
